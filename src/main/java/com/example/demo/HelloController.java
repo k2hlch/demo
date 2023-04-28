d9fce06cfebdd9beee64d9d60c94f9b52cfdb87d
k2hlch <kkh@ildong.com>
2023-04-28 14:20:19 +0900

    First hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello1() {
-        return "Hello from  Spring Apps Enterprise\n";
+        return "First Hello from  Spring Apps Enterprise\n";
     }
 }
